67811ada9a96d12ad33e2c5886facde1d65ab664
KseniiaBryzhko <114442785+KseniiaBryzhko@users.noreply.github.com>
2023-02-26 15:18:49 +0000

    Deploying to gh-pages from @ KseniiaBryzhko/goit-js-hw-09@9b9070985c436e8404524b26478445e9b74a46a0 🚀

---
+++ 01-color-switcher.78c67026.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.body,a=null;t.addEventListener("click",(function(){t.disabled=!0,a=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));n.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.78c67026.js.map
---
+++ 01-color-switcher.85d4afb1.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.body;let d=null;t.addEventListener("click",(()=>{t.disabled=!0,d=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;a.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(()=>{clearInterval(d),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.85d4afb1.js.map
